0ad18cd25c5875a994a94e0f264086c700f24358
krath1bb <brad@krathwohl.io>
2023-09-12 17:53:43 -0400

    added revo and rufus

--- windows/bootstrap.ps1
+++ windows/bootstrap.ps1
@@ -2,7 +2,7 @@
 #Set-ExecutionPolicy Bypass -Scope CurrentUser -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 ### Install Chocolatey Packages
-choco install -y adobereader amd-ryzen-chipset autodesk-fusion360 cpu-z cygwin discord displaycal epicgameslauncher everything firefox git github-desktop googlechrome googledrive icue lghub msiafterburner orcaslicer retroarch steam superslicer tailscalevlc vscode winrar winscp wiztree
+choco install -y adobereader amd-ryzen-chipset autodesk-fusion360 cpu-z cygwin discord displaycal epicgameslauncher everything firefox git github-desktop googlechrome googledrive icue lghub msiafterburner orcaslicer retroarch revo-uninstaller rufus steam superslicer tailscalevlc vscode winrar winscp wiztree
 
 ### Enable Hyper-V
 #Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
